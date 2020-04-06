d3436bdbcc8843059a804561449384725c16fe34
robaraneda <robaraneda@gmail.com>
2020-04-06 17:01:45 -0400

    se refactoriza el codigo stte

--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -10,6 +10,62 @@ use Illuminate\Http\Request;
 
 class StateController extends Controller
 {
+
+  protected function validateData()
+  {
+    return request()->validate([
+      'description' => 'required|max:255'
+    ]);
+  }
+
+  protected function responseSuccess($data)
+  {
+    return response()->json([
+      'success' => true,
+      'data' => $data,
+      'error' => null,
+      'statusCode' => 200
+    ]);
+  }
+
+  protected function responseException($exception)
+  {
+    return response()->json([
+      'success' => false,
+      'data' => null,
+      'error' => $exception->getMessage(),
+      'statusCode' => 500
+    ]);
+  }
+  protected function responseUnauthorized()
+  {
+    return response()->json([
+      'success' => false,
+      'data' => null,
+      'error' => 'Sin autorización',
+      'statusCode' => 401
+    ]);
+  }
+
+  protected function responseBadRequest()
+  {
+    return response()->json([
+      'success' => false,
+      'data' => null,
+      'error' => 'Url mal formada. Detente!!',
+      'statusCode' => 400
+    ]);
+  }
+
+  protected function responseNoContent()
+  {
+    return response()->json([
+      'success' => false,
+      'data' => null,
+      'error' => 'Elemento no encontrado',
+      'statusCode' => 204
+    ]);
+  }
   /**
    * Display a listing of the resource.
    *
@@ -18,28 +74,14 @@ class StateController extends Controller
   public function index()
   {
     try {
-      if (request()->isJson()) {
-        return response()->json([
-          'success' => true,
-          'data' => new StateCollection(State::all()),
-          'error' => null,
-          'statusCode' => 200
-        ], 200);
-      } else {
-        return response()->json([
-          'success' => false,
-          'data' => null,
-          'error' => 'Unauthorized',
-          'statusCode' => 401
-        ], 200);
-      }
+      if (!request()->isJson())
+        return $this->responseUnauthorized();
+
+      $states = new StateCollection(State::all());
+
+      return $this->responseSuccess($states);
     } catch (\Exception $exception) {
-      return response()->json([
-        'success' => false,
-        'data' => null,
-        'error' => $exception->getMessage(),
-        'statusCode' => 500
-      ], 200);
+      return $this->responseException($exception);
     }
   }
 
@@ -49,9 +91,22 @@ class StateController extends Controller
    * @param  \Illuminate\Http\Request  $request
    * @return \Illuminate\Http\Response
    */
-  public function store(Request $request)
+  public function store()
   {
-    //
+    try {
+      if (!request()->isJson())
+        return $this->responseUnauthorized();
+
+      $dataStore = $this->validateData();
+
+      $state = new State();
+
+      $state = $state->create($dataStore);
+
+      return $this->responseSuccess($state);
+    } catch (\Exception $exception) {
+      return $this->responseException($exception);
+    }
   }
 
   /**
@@ -62,7 +117,22 @@ class StateController extends Controller
    */
   public function show($id)
   {
-    //
+    try {
+      if (!request()->isJson())
+        return $this->responseUnauthorized();
+
+      if (!is_numeric($id))
+        return $this->responseBadRequest();
+
+      $data = State::whereId($id)->first();
+
+      if (!isset($state))
+        return $this->responseNoContent();
+
+      return $this->responseSuccess($data);
+    } catch (\Exception $exception) {
+      return $this->responseException($exception);
+    }
   }
 
   /**
@@ -91,39 +161,24 @@ class StateController extends Controller
   public function sections($idState)
   {
     try {
-      if (request()->isJson()) {
+      if (!request()->isJson())
+        return $this->responseUnauthorized();
 
-        $state = new StateResource(State::find($idState));
 
-        $state['sections'] = [
-          'href' => url('api/v2/states/' . $idState . '/sections'),
-          'rel' => 'sections',
-          'sections' => $state->sections->map->formatModel()
-        ];
+      $state = new StateResource(State::find($idState));
 
-        return response()->json([
-          'success' => true,
-          'data' => $state->sections,
-          'error' => null,
-          'statusCode' => 200
-        ], 200);
-      } else {
-        return response()->json([
-          'success' => false,
-          'data' => null,
-          'error' => 'Unauthorized',
-          'statusCode' => 401
-        ], 200);
-      }
+      $state['sections'] = [
+        'href' => url('api/v2/states/' . $idState . '/sections'),
+        'rel' => 'sections',
+        'sections' => $state->sections->map->formatModel()
+      ];
+
+      return $this->responseSuccess($state->sections);
     } catch (\Exception $exception) {
-      return response()->json([
-        'success' => false,
-        'data' => null,
-        'error' => $exception->getMessage(),
-        'statusCode' => 500
-      ], 200);
+      return $this->responseException($exception);
     }
   }
+
 
   public function workareas($idState)
   {
